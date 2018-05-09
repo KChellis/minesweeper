51b44d86bd802ef92d3f5b9724e402abd6156dfc
Cameron Button and Kristen Chellis <kristen@>
2018-05-09 09:01:11 -0700

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { TimerComponent } from './timer/timer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule
