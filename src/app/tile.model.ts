30dd6edc64ed5c8dd2df3fc03db1a372d3e3bb36
Kristen Chellis <peanutster@gmail.com>
2018-05-10 06:29:25 -0700

    add surrounding cells function to tile model

--- src/app/tile.model.ts
+++ src/app/tile.model.ts
@@ -1,4 +1,38 @@
+import {Board} from './board.model'
 export class Tile {
-  mineCount:  number = null;
+  mineCount:  number = nullTile
+  surroundingCells:Tile[] = [];
   constructor(public coordinates: number[], public value: number) {}
+
+  findSurrounding(board) {
+    let row = this.coordinates[0];
+    let col = this.coordinates[1];
+    if(row-1 > 0) {
+      this.surroundingCells.push(board[row-1][col]);
+      if(col-1>0){
+        this.surroundingCells.push(board[row-1][col-1]);
+      }
+      if(col+1 < board.width){
+        this.surroundingCells.push(board[row-1][col+1]);
+      }
+
+    }
+    if(row+1 < board.height) {
+      this.surroundingCells.push(board[row+1][col]);
+      if(col-1>0){
+        this.surroundingCells.push(board[row+1][col-1]);
+      }
+      if(col+1 < board.width){
+        this.surroundingCells.push(board[row+1][col+1]);
+      }
+
+    }
+    if(col-1>0){
+      this.surroundingCells.push(board[row][col-1]);
+    }
+    if(col+1 < board.width){
+      this.surroundingCells.push(board[row][col+1]);
+    }
+
+  }
 }
